8864de39971b7ee5f011195ae132fc86fb61830b
Giacomo Detomaso <giacomodetomaso24@gmail.com>
2026-08-02 15:39:49 +0200

    streamlined text generation module and overrided base_url property when inf. engine is vllm

--- core/utils/generator/ai_model_client_builder.py
+++ core/utils/generator/ai_model_client_builder.py
@@ -2,13 +2,25 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.language_models import BaseChatModel
 
-from settings import AIModelSettings
+from settings import AIModelSettings, settings
 
 
 def build_chat_model(ai_model_settings: AIModelSettings) -> BaseChatModel:
     """Instantiate the LangChain chat model wrapper matching `ai_model_settings.provider`."""
+    inference_mode = settings.workflow.inference.mode
+
+    gen_param_dict = ai_model_settings.generation_params
+
+    # Update the base_url param with the correct dynamically built one
+    if inference_mode == "vllm":
+        gen_param_dict.update({
+            "base_url": settings.workflow.inference.vllm.get_base_url()
+        })
+
     return init_chat_model(
         model=ai_model_settings.model_name,
         model_provider=ai_model_settings.provider,
-        **ai_model_settings.generation_params,
+        **gen_param_dict,
     )
+
+# TODO build the embedding model
--- core/utils/generator/text_generator.py
+++ core/utils/generator/text_generator.py
@@ -1,18 +1,58 @@
 """Invokes the generation chat model to produce a new malicious prompt."""
+from unittest.mock import Base
+
 from langchain_core.language_models import BaseChatModel
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage, BaseMessage
+from pydantic import BaseModel, ValidationError
 
 from core.models.dataset_generation_io_models import (
     InputAttackModel,
     InputDatasetModel,
-    OutputModel,
 )
-from core.utils.generator.prompt_builder import build_generation_messages
+
+from settings import settings
+
+_INTERNAL_RETRIES = settings.workflow.generation_schema_fix_retries
+
+def _build_generation_messages(source: InputDatasetModel, target: InputAttackModel) -> list[BaseMessage]:
+    prompt_settings = settings.prompts
+
+    user_content = prompt_settings.user.format(
+        original_prompt=source.original_prompt,
+        original_intent=source.original_intent,
+        target_intent=target.target_intent,
+        target_examples=target.target_examples,
+    )
+
+    return [
+        SystemMessage(content=prompt_settings.system),
+        HumanMessage(content=user_content),
+    ]
 
 
-class TextGenerator:
-    def __init__(self, chat_model: BaseChatModel):
-        self._structured_chat_model = chat_model.with_structured_output(OutputModel)
+def generate(
+    chat_model: BaseChatModel,
+    source: InputDatasetModel,
+    target: InputAttackModel,
+    output_schema: type[BaseModel]
+) -> type[BaseModel]:
+    llm = chat_model.with_structured_output(output_schema)
 
-    def generate(self, source: InputDatasetModel, target: InputAttackModel) -> OutputModel:
-        messages = build_generation_messages(source, target)
-        return self._structured_chat_model.invoke(messages)
+    messages = _build_generation_messages(source, target)
+
+    retries = 0
+
+    while retries < _INTERNAL_RETRIES:
+        try:
+            response = llm.invoke(messages)
+
+            # No validation errors, break the loop
+            break
+        except ValidationError as e:
+            messages.append(AIMessage(response))
+            messages.append(HumanMessage("The provided response does not satisfy the output schema. Read the error and fix it."))
+            messages.append(HumanMessage(e.json(indent=4)))
+
+            retries += 1
+
+    return response
