901538196887eb9d927c9a8c14ea9593639df8ce
Giacomo Detomaso <giacomodetomaso24@gmail.com>
2026-08-02 22:50:36 +0200

    added simple main

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import asyncio
 
-# 3. Load the model and tokenizer directly onto the CPU
-print("Loading model onto CPU (this may take a moment to compress weights)...")
+from dataset_agent.graph import build_and_compile_graph, get_graph_initial_state
+from dataset_agent.core.inference.inference_context_manager import inference_environment
 
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-8B")
+from settings import settings
+
+def generate_dataset() -> None:
+    initial_state = get_graph_initial_state()
+    graph = build_and_compile_graph()
+
+    # Wrap graph execution inside the environment
+    with inference_environment():
+        graph.invoke(initial_state)
+
+
+async def generate_dataset_async() -> None:
+    initial_state = get_graph_initial_state()
+    graph = build_and_compile_graph()
+    
+    # Wrap graph execution inside the environment
+    with inference_environment():
+        graph.ainvoke(initial_state)
+
+def main() -> None:
+    if settings.workflow.inference.mode == "vllm" and settings.workflow.inference.vllm.invoke_mode == "async":
+        asyncio.run(generate_dataset_async())
+    else:
+        generate_dataset()
+
+if __name__ == "__main__":
+    main()
