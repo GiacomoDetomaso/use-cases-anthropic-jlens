5cad3c4dfd76072e347d40bb1868a1d86a3ebd20
Giacomo Detomaso <giacomodetomaso24@gmail.com>
2026-08-05 22:23:13 +0200

    removed wrong import

--- dataset_agent/core/inference/inference_context_manager.py
+++ dataset_agent/core/inference/inference_context_manager.py
@@ -5,7 +5,6 @@ from dataset_agent.core.inference.vllm import start_vllm_server
 
 from dataset_agent.core.llm.ai_model_client_builder import (
     get_chat_model, 
-    get_validation_chat_model
 )
 
 from loguru import logger
@@ -26,7 +25,6 @@ def inference_environment():
 
             # Clear cached models if server restarts
             get_chat_model.cache_clear()
-            get_validation_chat_model.cache_clear()
     else:
         # Direct LangChain mode (No server process needed)
         yield None
